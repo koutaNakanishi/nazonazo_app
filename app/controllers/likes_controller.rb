3c95b108f3c821610d5279ee519477083dc9c905
vagrant <nizi26i26@gmail.com>
2018-06-22 09:46:13 +0100

    良いねボタンを追加！！

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,17 @@
 class LikesController < ApplicationController
 	def create
+		@nazo=Nazo.find(params[:nazo_id])
 		@like=Like.create(user_id: current_user.id,nazo_id: params[:nazo_id])
-		@likes=Like.where(nazo_id: params[:nazo_id])
-		@nazos=Nazo.all
+		#@likes=Like.where(nazo_id: params[:nazo_id])
+		#@nazos=Nazo.all
 	end
 
 	def destroy
+		@nazo=Nazo.find(params[:nazo_id])
 		like=Like.find_by(user_id: current_user.id,nazo_id: params[:nazo_id])
 		like.destroy
-		@likes=like.where(nazo_id: params[:nazo_id])
-		@nazos=Nazo.all
+		#@likes=like.where(nazo_id: params[:nazo_id])
+		#@nazos=Nazo.all
 	end
 
 end
